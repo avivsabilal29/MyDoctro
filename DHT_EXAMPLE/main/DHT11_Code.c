0b11897d5041c7ad339dd5cf3744ba7e69dfde4f
Muhammad Aviv Sabilal <89498280+avivsabilal29@users.noreply.github.com>
2023-04-12 06:38:16 +0700

    Add files via upload

---
+++ DHT_EXAMPLE/main/DHT11_Code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "DHT11.h"
+void app_main()
+{
+    DHT11_init(GPIO_NUM_4);
+
+    while(1) {
+        printf("Temperature is %d \n", DHT11_read().temperature);
+        printf("Humidity is %d\n", DHT11_read().humidity);
+        printf("Status code is %d\n", DHT11_read().status);
+        waitSeconds(1);
+    }
+}
